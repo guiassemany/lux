7e5c447cfe0f0b82411fa30013ae2ce3b902c7b5
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-01-28 20:08:29 -0200

    Include HTTP Foundation

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-$input = $_GET['name'];
+require_once __DIR__.'/vendor/autoload.php';
 
-printf('Hello %s', $input);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
