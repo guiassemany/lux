5a35b26fb9951fc88f93cc747c9abd8d310bd847
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-01-28 20:52:26 -0200

    Include Routing Component

---
+++ src/app.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => 'render_template',
+)));
+
+$routes->add('bye', new Routing\Route('/bye'));
+
+return $routes;
---
+++ src/pages/hello.php
@@ -0,0 +1,1 @@
+<h1>Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?></h1>
