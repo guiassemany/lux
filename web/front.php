4a134740204009760055c5982986e245cf7876cb
Guilherme Assemany <guilhermeassemany@hotmail.com>
2016-01-29 23:39:09 -0200

    Configuring Event listeners

--- web/front.php
+++ web/front.php
@@ -1,21 +1,17 @@
 <?php
 
-// example.com/web/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-function render_template(Request $request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+$dispatcher = new EventDispatcher();
 
-    return new Response(ob_get_clean());
-}
+//$dispatcher->addSubscriber(new Lux\ContentLengthListener());
+//$dispatcher->addSubscriber(new Lux\GoogleListener());
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -24,7 +20,8 @@ $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
-$framework = new Lux\Framework($matcher, $resolver);
+
+$framework = new Lux\Framework($dispatcher, $matcher, $resolver);
 $response = $framework->handle($request);
 
 $response->send();
